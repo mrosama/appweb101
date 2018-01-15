0636da082ba3902d10580590a14149ad463720eb
Osama Salama <osama_eg@outlook.com>
2018-01-15 20:24:52 +0200

    ww

--- home.php
+++ home.php
@@ -1,7 +1,5 @@
 <?php
 
-<<<<<<< HEAD
-
 $x = true ;
 
 if($x == true){
@@ -10,6 +8,5 @@ echo "ok...";
 else {
     echo "Error...";
 }
-=======
+
 echo "welcome first php";
->>>>>>> c42611a75876ab7b6bc2e377b39c99b6077442ae
